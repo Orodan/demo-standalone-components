2d15476043f2b4c9ee6e053b7278e287c960f3aa
Orodan <jimmykasprzak@gmail.com>
2022-06-13 23:14:45 +0200

    ✨ remove group-detail module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'group-detail/:name', loadChildren: () => import('./group-detail/group-detail.module').then(f => f.GroupDetailModule) }
+  { path: 'group-detail/:name', loadComponent: () => import('./group-detail/group-detail.component').then(f => f.GroupDetailComponent) }
 ];
 
 @NgModule({
